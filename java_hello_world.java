6b26d866d6b06239b74edbf2cb393bfb634c472e
PRATIK GARG <45739182+pratik-garg@users.noreply.github.com>
2020-10-03 16:04:10 +0530

    Create java_hello_world.java
    hello world in java language

---
+++ java_hello_world.java
@@ -0,0 +1,9 @@
+class Helloworld{
+  public void greet{
+    System.out.println("HELLO_WORLD");
+    }
+  public static void main(String args[]){
+    Helloworld h1 = new Helloworld();
+    h1.greet();
+  }
+}
